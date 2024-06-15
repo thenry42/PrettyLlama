47393e23e4850c9b1d3c6bbc13d210322d5e752f
thenry42 <h_thibault@yahoo.fr>
2024-06-15 20:17:53 +0200

    install okay

--- install.sh
+++ install.sh
@@ -9,27 +9,27 @@ fi
 # Define variables
 EXECUTABLE_NAME="PrettyLlama"
 DESKTOP_FILE_NAME="${EXECUTABLE_NAME}.desktop"
-LOGO_FILE_NAME="/utils/${EXECUTABLE_NAME}.png"
+LOGO_FILE_NAME="${EXECUTABLE_NAME}.png"
 
 EXECUTABLE_PATH="/usr/bin/${EXECUTABLE_NAME}"
-DESKTOP_FILE_PATH="${HOME}/Desktop/${DESKTOP_FILE_NAME}"
-LOGO_PATH="/usr/share/icons/${EXECUTABLE_NAME}"
+DESKTOP_FILE_PATH="/usr/share/applications/${DESKTOP_FILE_NAME}"
+LOGO_PATH="/usr/share/icons/"
 
 # Copy executable to /usr/bin
 cp "${EXECUTABLE_NAME}" "${EXECUTABLE_PATH}"
 chmod +x "${EXECUTABLE_PATH}"
 
 # Copy Logo File
-cp "${LOGO_FILE_NAME}" "${LOGO_PATH}"
-chmod +x "${LOGO_PATH}"
+cp "./utils/${LOGO_FILE_NAME}" "${LOGO_PATH}"
+chmod +x "${LOGO_PATH}/${LOGO_FILE_NAME}"
 
 # Create .desktop file
 cat << EOF > "${DESKTOP_FILE_PATH}"
 [Desktop Entry]
 Name=PrettyLlama
 Comment=Simple CLI program to use ollama models in a nice looking way
-Exec=${EXECUTABLE_NAME}
-Icon=${LOGO_PATH}
+Exec=gnome-terminal --full-screen -- ${EXECUTABLE_NAME}
+Icon=${LOGO_PATH}/${LOGO_FILE_NAME}
 Type=Application
 EOF
 
